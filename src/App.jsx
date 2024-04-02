d9f1dfa6e61939be05eddcda4ec8812b591ef6bc
slimpeas <reitin.pro@gmail.com>
2024-04-02 10:49:57 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import './App.css'
 
 function App() {
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+  }
   return (
     <>
       <h1>Bienvenue chez nous !</h1>
+      <h2>Connectez vous</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="" id="" placeholder='Entrez votre prénom...'/>
+        <button type="submit">Accéder à votre espace</button>
+      </form>
     </>
   )
 }
