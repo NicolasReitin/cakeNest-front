5a9f4c24905bd33c6bf98b751cb522362adcce2b
slimpeas <reitin.pro@gmail.com>
2024-04-02 12:08:18 +0200

    F01 - done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import './assets/css/App.css'
+import Login from '@/Components/Login'
+
 
 function App() {
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert('Bonjour ' + e.target[0].value)
-    e.target.reset();
-  }
+  
   return (
     <>
-      <h1>Bienvenue chez nous !</h1>
-      <h2>Connectez vous</h2>
-
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="" placeholder='Entrez votre prénom...' required/>
-        <button type="submit">Accéder à votre espace</button>
-      </form>
+      <Login />
     </>
   )
 }
